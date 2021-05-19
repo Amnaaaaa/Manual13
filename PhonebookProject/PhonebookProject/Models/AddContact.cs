63af9fce38fee70d1cd087fc50d32a01d8b5d8b0
Amnaaaaa <81854874+Amnaaaaa@users.noreply.github.com>
2021-05-19 17:40:32 +0500

    COMMIT

---
+++ PhonebookProject/PhonebookProject/Models/AddContact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhonebookProject.Models
+{
+    public class AddContact
+    {
+        public String name { get; set; }
+        private String email { get; set; }
+        private String contactNumber { get; set; }
+
+
+        
+
+    }
+}
