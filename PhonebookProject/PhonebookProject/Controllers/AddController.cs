94603f2da8a7252ce056b9cdacb89ee42e779573
Amnaaaaa <81854874+Amnaaaaa@users.noreply.github.com>
2021-05-19 17:44:46 +0500

    COMMIT

--- PhonebookProject/PhonebookProject/Controllers/AddController.cs
+++ PhonebookProject/PhonebookProject/Controllers/AddController.cs
@@ -12,6 +12,61 @@ namespace PhonebookProject.Controllers
         public ActionResult Index()
         {
             return View();
+            public List<Contact> getAllContacts()
+            {
+                return contacts;
+            }
+
+
+
+            public List<Contact> searchBasedOnEmail(String emailIdText)
+            {
+                List<Contact> searched = new ArrayList<Contact>();
+
+                for (int i = 0; i < contacts.size(); i++)
+                {
+                    if (contacts.get(i).getEmail().contains(emailIdText))
+                    {
+                        searched.add(contacts.get(i));
+                    }
+                }
+                return searched;
+            }
+
+
+
+            public List<Contact> searchBasedOnName(String nameText)
+            {
+                List<Contact> searched = new ArrayList<Contact>();
+
+                for (int i = 0; i < contacts.size(); i++)
+                {
+                    if (contacts.get(i).getName().contains(nameText))
+                    {
+                        searched.add(contacts.get(i));
+                    }
+                }
+                return searched;
+            }
+
+
+            public List<Contact> searchBasedOnContact(String contacText)
+            {
+                List<Contact> searched = new ArrayList<Contact>();
+
+                for (int i = 0; i < contacts.size(); i++)
+                {
+                    if (contacts.get(i).getContactNumber().contains(contacText))
+                    {
+                        searched.add(contacts.get(i));
+                    }
+                }
+                return searched;
+
+
+
+
+
+            }
         }
-    }
 }
--- PhonebookProject/PhonebookProject/Models/Contact.cs
+++ PhonebookProject/PhonebookProject/Models/Contact.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Date;
 
 namespace PhonebookProject.Models
 {
     public class Contact
     {
+        private String name { get; set; }
+        private Date dob { get; set; }
+        private String contactNumber { get; set; }
+        private String email { get; set; }
+
+        private Date savedOn { get; set; }
+
     }
 }
